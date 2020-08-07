5767f89174eaeed9c905fc6edd2143bcf64ee480
paddy <yoosinpaddyrop@gmail.com>
2020-08-07 11:28:56 +0300

    aa

--- confirmEmail.php
+++ confirmEmail.php
@@ -1,11 +1,13 @@
 
 <?php
+require "paddymailer/src/PHPMailer.php";
 $servername = "localhost";
 $username = "transdua_cars";
 $password = "WBuChjJrqApt59Z";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=transdua_cars", $username, $password);
+  // set the PDO error mode to exception
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -57,23 +59,54 @@ if($userEmail){
   //  $mail->Subject = 'Email Verification';   //Sets the Subject of the message
   //  $mail->Body = $mail_body;       //An HTML or plain text message body
 
-  $to = $userEmail;
-  $subject = "Password recovery";
+  // $to = $userEmail;
+  // $subject = "Password recovery";
   
-  $message = "<b>Here is your token.</b>";
-  $message .= "<b>This is headline.</b>";
+  // $message = "<b>Here is your token.</b>";
+  // $message .= "<b>This is headline.</b>";
   
-  $header = "From:info@itsmywriter.com \r\n";
-  $header .= "MIME-Version: 1.0\r\n";
-  $header .= "Content-type: text/html\r\n";
+  // $header = "From:info@itsmywriter.com \r\n";
+  // $header .= "MIME-Version: 1.0\r\n";
+  // $header .= "Content-type: text/html\r\n";
   
-  $retval = mail ($to,$subject,$message,$header);
-  if($retval){
+  // $retval = mail ($to,$subject,$message,$header);
+  // if($retval){
+  //   $response = ['status_code'=> '200', 'message'=>'Email sent, check email for verification code', 'useremail'=>$usersData[0]['email'],'userpassword'=>$usersData[0]['password'] ];
+  //   echo(json_encode($response));
+  // }else{       
+  //   $response = ['status_code'=> '400', 'message'=>'Coulnt send email', 'useremail'=>$usersData[0]['email'],'userpassword'=>$usersData[0]['password'] ];
+  //   echo(json_encode($response));
+  // }
+
+$mail = new PHPMailer; //$mail->SMTPDebug = 3;      // Enable verbose debug output
+$mail->isSMTP();     // Set mailer to use SMTP
+$mail->Host = ‘mail.example.com;servername.truehost.cloud’;  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;   // Enable SMTP authentication
+$mail->Username = ‘support@itsmywriter.com’;     // SMTP username
+$mail->Password = 2Q^yp3X]i!_H;              // SMTP password
+$mail->SMTPSecure = ‘tls’;        // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;      // TCP port to connect to or 25 for non secure
+$mail->setFrom('support@itsmywriter.com', ‘Mailer’);
+$mail->addAddress($userEmail);     // Add a recipient
+// $mail->addAddress(‘ellen@example.com’);               // Name is optional
+// $mail->addReplyTo(‘info@example.com’, ‘Information’);
+// $mail->addCC(‘cc@example.com’);
+// $mail->addBCC(‘bcc@example.com’);
+// $mail->addAttachment(‘/var/tmp/file.tar.gz’);         // Add attachments
+// $mail->addAttachment(‘/tmp/image.jpg’, ‘new.jpg’);    // Optional name
+$mail->isHTML(true);            // Set email format to HTML
+$mail->Subject = ‘Here is the subject’;
+$mail->Body    = ‘This is the HTML message body <b>in bold!</b>’;
+$mail->AltBody = ‘This is the body in plain text for non-HTML mail clients’;
+// if(!$mail->send()) {    echo ‘Message could not be sent.’;    echo ‘Mailer Error: ‘ .
+// $mail->ErrorInfo;} else {    echo ‘Message has been sent’;}
+// }
+if($mail->send())
     $response = ['status_code'=> '200', 'message'=>'Email sent, check email for verification code', 'useremail'=>$usersData[0]['email'],'userpassword'=>$usersData[0]['password'] ];
     echo(json_encode($response));
   }else{       
-    $response = ['status_code'=> '400', 'message'=>'Coulnt send email', 'useremail'=>$usersData[0]['email'],'userpassword'=>$usersData[0]['password'] ];
+    $response = ['status_code'=> '400', 'message'=>'Coulnt send email'.$mail->ErrorInfo, 'useremail'=>$usersData[0]['email'],'userpassword'=>$usersData[0]['password'] ];
     echo(json_encode($response));
   }
-}
+
 ?>
