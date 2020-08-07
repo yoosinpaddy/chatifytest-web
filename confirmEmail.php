dd1fc40708f82da2805ef3142aa895b019f02080
paddy <yoosinpaddyrop@gmail.com>
2020-08-07 10:41:04 +0300

    password recovery

--- confirmEmail.php
+++ confirmEmail.php
@@ -34,37 +34,46 @@ if($userEmail){
 
 	
 
-   $mail_body = "
-   <p>Hi ,</p>
-   <p>User ".$userEmail.", has requested for password reset</p>
-   ";
-   require 'class/class.phpmailer.php';
-   $mail = new PHPMailer;
-   $mail->IsSMTP();        //Sets Mailer to send message using SMTP
-   $mail->Host = 'mail.itsmywriter.com';  //Sets the SMTP hosts of your Email hosting, this for Godaddy
-   $mail->Port = '80';        //Sets the default SMTP server port
-   $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
-   $mail->Username = 'support@itsmywriter.com';     //Sets SMTP username
-   $mail->Password = '2Q^yp3X]i!_H';     //Sets SMTP password
-   $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
-   $mail->From = 'info@itsmywriter.com';   //Sets the From email address for the message
-   $mail->FromName = 'Webslesson';     //Sets the From name of the message
-   $mail->AddAddress('paddykiprop@gmail.com');  //Adds a "To" address      
-   $mail->To='paddykiprop@gmail.com';  //Adds a "To" address   
+  //  $mail_body = "
+  //  <p>Hi ,</p>
+  //  <p>User ".$userEmail.", has requested for password reset</p>
+  //  ";
+  //  require 'class/class.phpmailer.php';
+  //  $mail = new PHPMailer;
+  //  $mail->IsSMTP();        //Sets Mailer to send message using SMTP
+  //  $mail->Host = 'mail.itsmywriter.com';  //Sets the SMTP hosts of your Email hosting, this for Godaddy
+  //  $mail->Port = '80';        //Sets the default SMTP server port
+  //  $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
+  //  $mail->Username = 'support@itsmywriter.com';     //Sets SMTP username
+  //  $mail->Password = '2Q^yp3X]i!_H';     //Sets SMTP password
+  //  $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
+  //  $mail->From = 'info@itsmywriter.com';   //Sets the From email address for the message
+  //  $mail->FromName = 'Webslesson';     //Sets the From name of the message
+  //  $mail->AddAddress('paddykiprop@gmail.com');  //Adds a "To" address      
+  //  $mail->To='paddykiprop@gmail.com';  //Adds a "To" address   
 
-   $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
-   $mail->IsHTML(true);       //Sets message type to HTML    
-   $mail->Subject = 'Email Verification';   //Sets the Subject of the message
-   $mail->Body = $mail_body;       //An HTML or plain text message body
-   if($mail->Send())        //Send an Email. Return true on success or false on error
-   {
+  //  $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
+  //  $mail->IsHTML(true);       //Sets message type to HTML    
+  //  $mail->Subject = 'Email Verification';   //Sets the Subject of the message
+  //  $mail->Body = $mail_body;       //An HTML or plain text message body
 
+  $to = $userEmail;
+  $subject = "Password recovery";
+  
+  $message = "<b>Here is your token.</b>";
+  $message .= "<b>This is headline.</b>";
+  
+  $header = "From:info@itsmywriter.com \r\n";
+  $header .= "MIME-Version: 1.0\r\n";
+  $header .= "Content-type: text/html\r\n";
+  
+  $retval = mail ($to,$subject,$message,$header);
+  if($retval){
     $response = ['status_code'=> '200', 'message'=>'Email sent, check email for verification code', 'useremail'=>$usersData[0]['email'],'userpassword'=>$usersData[0]['password'] ];
     echo(json_encode($response));
-   }else{
-       
+  }else{       
     $response = ['status_code'=> '400', 'message'=>'Coulnt send email', 'useremail'=>$usersData[0]['email'],'userpassword'=>$usersData[0]['password'] ];
     echo(json_encode($response));
-   }
+  }
 }
 ?>
