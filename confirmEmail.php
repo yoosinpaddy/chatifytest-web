d923617d8baa860364c3058216842d343aaf7fe0
paddy <yoosinpaddyrop@gmail.com>
2020-08-07 14:02:48 +0300

    ZXAs

--- confirmEmail.php
+++ confirmEmail.php
@@ -1,12 +1,5 @@
 <?php
-require 'paddymailer/src/Exception.php';
-require 'paddymailer/src/PHPMailer.php';
-require 'paddymailer/src/SMTP.php';
 require 'sendmail.php';
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 $servername = "localhost";
 $username = "transdua_cars";
 $password = "WBuChjJrqApt59Z";
@@ -42,26 +35,26 @@ if($userEmail){
   // echo "llll";
 
 
-$mail = new PHPMailer\PHPMailer\PHPMailer(); //$mail->SMTPDebug = 3;      // Enable verbose debug output
-$mail->isSMTP();     // Set mailer to use SMTP
-$mail->Host = "mail.itsmywriter.com";  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;   // Enable SMTP authentication
-$mail->Username = 'support@itsmywriter.com';     // SMTP username
-$mail->Password = '2Q^yp3X]i!_H';              // SMTP password
-$mail->SMTPSecure = 'tls';        // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;      // TCP port to connect to or 25 for non secure
-$mail->setFrom('support@itsmywriter.com', 'Its my writer');
-$mail->addAddress($userEmail);     // Add a recipient
+// $mail = new PHPMailer\PHPMailer\PHPMailer(); //$mail->SMTPDebug = 3;      // Enable verbose debug output
+// $mail->isSMTP();     // Set mailer to use SMTP
+// $mail->Host = "mail.itsmywriter.com";  // Specify main and backup SMTP servers
+// $mail->SMTPAuth = true;   // Enable SMTP authentication
+// $mail->Username = 'support@itsmywriter.com';     // SMTP username
+// $mail->Password = '2Q^yp3X]i!_H';              // SMTP password
+// $mail->SMTPSecure = 'tls';        // Enable TLS encryption, `ssl` also accepted
+// $mail->Port = 465;      // TCP port to connect to or 25 for non secure
+// $mail->setFrom('support@itsmywriter.com', 'Its my writer');
+// $mail->addAddress($userEmail);     // Add a recipient
 // $mail->addAddress('ellen@example.com');               // Name is optional
 // $mail->addReplyTo('info@example.com', 'Information');
 // $mail->addCC('cc@example.com');
 // $mail->addBCC('bcc@example.com');
 // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);            // Set email format to HTML
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+// $mail->isHTML(true);            // Set email format to HTML
+// $mail->Subject = 'Here is the subject';
+// $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 // if(!$mail->send()) {    echo 'Message could not be sent.';    echo 'Mailer Error: ' .
 // $mail->ErrorInfo;} else {    echo 'Message has been sent';}
 // }
